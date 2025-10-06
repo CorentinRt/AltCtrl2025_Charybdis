edb88957ff25abb8c90c5a9c7a12397a70fb4e60
Corentinrmt <corentin.remot@gmail.com>
2025-10-06 12:36:00 +0200

    [ADD] proto move ships

--- Assets/_project/Scripts/Ships/SO_ShipData.cs
+++ Assets/_project/Scripts/Ships/SO_ShipData.cs
@@ -8,15 +8,23 @@ namespace AltCtrl.Charybdis
         #region Fields
         [Header("Movements")]
         [SerializeField] private float _acceleration;
+        [SerializeField] private float _minSpeed;
         [SerializeField] private float _maxSpeed;
+
         [SerializeField] private float _rotateSpeed;
+        [SerializeField] private float _maxRotateSpeed;
+        [SerializeField] private float _decelerationForce;
 
         #endregion
 
         #region Properties
         public float Acceleration => _acceleration;
+        public float MinSpeed => _minSpeed;
         public float MaxSpeed => _maxSpeed;
+
         public float RotateSpeed => _rotateSpeed;
+        public float MaxRotateSpeed => _maxRotateSpeed;
+        public float DecelerationForce => _decelerationForce;
 
         #endregion
 
--- Assets/_project/Scripts/Ships/ShipBehaviour.cs
+++ Assets/_project/Scripts/Ships/ShipBehaviour.cs
@@ -15,6 +15,9 @@ namespace AltCtrl.Charybdis
         [Header("Datas")]
         [SerializeField] private SO_ShipData _data;
 
+
+        private float _currentGouvernailInput;
+
         #endregion
 
 
@@ -23,17 +26,53 @@ namespace AltCtrl.Charybdis
 
         #endregion
 
+        private void FixedUpdate()
+        {
+            Move();
+
+            Rotate();
+        }
 
         private void Move()
         {
-            float deltaInput = Input.GetAxis("Horizontal");
+            float deltaInputVertical = Input.GetAxis("Vertical");
 
-            if (deltaInput != 0f)
+            Vector3 tempVelocity = _rb.linearVelocity;
+
+            tempVelocity += transform.up * Time.fixedDeltaTime * _data.Acceleration;
+
+
+            if (tempVelocity.magnitude * deltaInputVertical < _data.MinSpeed)
             {
+                tempVelocity = tempVelocity.normalized;
 
+                tempVelocity *= _data.MinSpeed;
+            }
+            else
+            {
+                tempVelocity *= deltaInputVertical;
+                tempVelocity = Vector3.ClampMagnitude(tempVelocity, _data.MaxSpeed);
             }
 
+            _rb.linearVelocity = tempVelocity;
+        }
 
+        private void Rotate()
+        {
+            float deltaInputHorizontal = Input.GetAxis("Horizontal");
+
+            if (deltaInputHorizontal != 0f)
+            {
+                _currentGouvernailInput += deltaInputHorizontal * Time.fixedDeltaTime * _data.RotateSpeed;
+            }
+            else
+            {
+                _currentGouvernailInput = Mathf.Lerp(_currentGouvernailInput, 0f, Time.fixedDeltaTime * _data.DecelerationForce);
+            }
+
+            _currentGouvernailInput = Mathf.Clamp(_currentGouvernailInput, -_data.MaxRotateSpeed, _data.MaxRotateSpeed);
+
+            _rb.angularVelocity = -_currentGouvernailInput;
         }
 
     }
