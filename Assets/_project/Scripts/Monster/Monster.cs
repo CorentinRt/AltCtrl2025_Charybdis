f666fbe46abd6a8bef1a4e51ebc784b764a76556
Manon Wimmer <manonwimmer30@gmail.com>
2025-10-06 14:59:38 +0200

    [ADD] Clamp monster movement to screen limits

--- Assets/_project/Scripts/Monster/Monster.cs
+++ Assets/_project/Scripts/Monster/Monster.cs
@@ -9,6 +9,8 @@ namespace AltCtrl.Charybdis
         [SerializeField] private float _moveSpeed = 5f;
         [SerializeField] private float _smoothTime = 0.1f;
 
+        [SerializeField] private float objectWidth, objectHeight;
+
         private Vector2 _moveDirection;
         private Vector2 _currentVelocity;
         private Vector2 _targetPosition;
@@ -16,11 +18,15 @@ namespace AltCtrl.Charybdis
         private bool _isMoving;
 
         private Rigidbody2D _rb;
+
+        private Vector2 screenBounds;
         // ----- FIELDS ----- //
 
         private void Start()
         {
             _rb = GetComponent<Rigidbody2D>();
+
+            screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
 
             if (InputManager.Instance == null) return;
 
@@ -38,6 +44,10 @@ namespace AltCtrl.Charybdis
             Debug.Log(mousePos);
             Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(mousePos);
             _targetPosition = new Vector2(mouseWorldPos.x, mouseWorldPos.y);
+
+             // Clamp to screen limits
+            _targetPosition.x = Mathf.Clamp(_targetPosition.x, -screenBounds.x + objectWidth, screenBounds.x - objectWidth);
+            _targetPosition.y = Mathf.Clamp(_targetPosition.y, -screenBounds.y + objectHeight, screenBounds.y - objectHeight);
         }
 
         void FixedUpdate()
