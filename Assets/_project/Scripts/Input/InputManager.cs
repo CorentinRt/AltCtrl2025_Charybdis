78886e6086937c0516431b93ac5f2017342c3108
Manon Wimmer <manonwimmer30@gmail.com>
2025-10-07 14:49:08 +0200

    [UPDATE] Option jostick control for monster movement

--- Assets/_project/Scripts/Input/InputManager.cs
+++ Assets/_project/Scripts/Input/InputManager.cs
@@ -29,10 +29,11 @@ public class InputManager : MonoBehaviour
         if (context.performed || context.canceled)
         {
             Vector2 moveDirection = context.ReadValue<Vector2>();
+            Debug.Log(moveDirection);
             OnMoveMonsterPressed?.Invoke(moveDirection);
         }
     }
-
+    
     public void MoveMonsterTempPressed(InputAction.CallbackContext context)
     {
         if (context.performed || context.canceled)
