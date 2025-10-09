4f03b4191b48b094be21382619d91687d0c8a69c
Manon Wimmer <manonwimmer30@gmail.com>
2025-10-09 11:42:44 +0200

    [ADD] Main menu - Add change volume slider + play with totems inputs

--- Assets/_project/Scripts/UI/MainMenuManager.cs
+++ Assets/_project/Scripts/UI/MainMenuManager.cs
@@ -1,7 +1,10 @@
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.Localization;
 using UnityEngine.Localization.Components;
 using UnityEngine.Localization.SmartFormat.PersistentVariables;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 namespace AltCtrl.Charybdis
 {
@@ -9,10 +12,14 @@ namespace AltCtrl.Charybdis
     {
         // ----- FIELDS ----- //
         [Header("Language")]
-        public LocalizeStringEvent _languageLocalizeStringEvent;
+        [SerializeField] private LocalizeStringEvent _languageLocalizeStringEvent;
 
         [Header("Microphone")]
-        public LocalizeStringEvent _microphoneLocalizeStringEvent;
+        [SerializeField] private LocalizeStringEvent _microphoneLocalizeStringEvent;
+
+        [Header("Volume")]
+        [SerializeField] private Slider _volumeSlider;
+        [SerializeField] private AudioMixer _audioMixer;
         // ----- FIELDS ----- //
 
         private void Start()
@@ -20,10 +27,19 @@ namespace AltCtrl.Charybdis
             UpdateLanguageTxt();
             UpdateMicrophoneTxt();
 
+            SetupVolumeSlider();
+
             if (InputManager.Instance != null)
             {
                 InputManager.Instance.OnNextMicrophonePressed += SetNextMicrophone;
                 InputManager.Instance.OnNextLanguagePressed += SetNextLanguage;
+
+                InputManager.Instance.OnTotem1Pressed += Play;
+                InputManager.Instance.OnTotem2Pressed += Play;
+                InputManager.Instance.OnTotem3Pressed += Play;
+
+                _volumeSlider.onValueChanged.AddListener(UpdateAudioMixerVolume);
+                InputManager.Instance.OnMoveRadioRotatorPressed += ChangeVolumeWithRadio; 
             }
         }
 
@@ -33,9 +49,18 @@ namespace AltCtrl.Charybdis
             {
                 InputManager.Instance.OnNextMicrophonePressed -= SetNextMicrophone;
                 InputManager.Instance.OnNextLanguagePressed -= SetNextLanguage;
+
+                InputManager.Instance.OnTotem1Pressed -= Play;
+                InputManager.Instance.OnTotem2Pressed -= Play;
+                InputManager.Instance.OnTotem3Pressed -= Play;
+
+                _volumeSlider.onValueChanged.RemoveListener(UpdateAudioMixerVolume);
+                InputManager.Instance.OnMoveRadioRotatorPressed -= ChangeVolumeWithRadio;
+
             }
         }
 
+        #region Language
         private void UpdateLanguageTxt()
         {
             string currentLanguage = UnityEngine.Localization.Settings.LocalizationSettings.SelectedLocale.Identifier.CultureInfo.NativeName;
@@ -61,8 +86,9 @@ namespace AltCtrl.Charybdis
 
             UpdateLanguageTxt();
         }
+        #endregion
 
-
+        #region Microphone
         private void UpdateMicrophoneTxt()
         {
             var localizedString = _microphoneLocalizeStringEvent.StringReference;
@@ -79,5 +105,41 @@ namespace AltCtrl.Charybdis
             InputManager.Instance.SetNextMicrophone();
             UpdateMicrophoneTxt();
         }
+        #endregion
+
+        #region Play
+        public void Play(bool pressed)
+        {
+            if (!pressed) return;
+            Play();
+        }
+
+        public void Play()
+        {
+            SceneManager.LoadScene("MainGame");
+        }
+        #endregion
+
+        #region Volume
+        public void SetupVolumeSlider()
+        {
+            _volumeSlider.maxValue = 100f;
+            _volumeSlider.value = 100f; 
+        }
+
+        public void ChangeVolumeWithRadio(int value)
+        {
+            _volumeSlider.value += value;
+
+            UpdateAudioMixerVolume(_volumeSlider.value);
+        }
+
+        public void UpdateAudioMixerVolume(float value)
+        {
+            float volume = Mathf.Clamp(value, 0.0001f, 100f); // éviter log(0)
+            float dB = Mathf.Log10(volume / 100f) * 80f; // dB entre -80 et 0
+            _audioMixer.SetFloat("MasterVolume", dB);
+        }
+        #endregion Volume
     }
 }
