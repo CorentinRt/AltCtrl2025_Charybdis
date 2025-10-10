64ca9a5d19860cbf4f781acd67224574f446d6d9
Manon Wimmer <manonwimmer30@gmail.com>
2025-10-10 15:30:51 +0200

    [ADD] Island vortex integration

--- Assets/_project/Scripts/Totems/Storm.cs
+++ Assets/_project/Scripts/Totems/Storm.cs
@@ -6,6 +6,15 @@ namespace AltCtrl.Charybdis
     [RequireComponent(typeof(Collider2D))]
     public class Storm : MonoBehaviour
     {
+        // ----- FIELDS ----- //
+        [SerializeField] private Collider2D _collider;
+
+        public Collider2D Collider { get => _collider; set => _collider = value; }
+        // ----- FIELDS ----- //
+
+
+
+        #region Trigger Enter / Exit
         private void OnTriggerEnter2D(Collider2D collision)
         {
             if (collision == null || collision.gameObject == null)
@@ -27,5 +36,6 @@ namespace AltCtrl.Charybdis
                 collision.gameObject.GetComponent<IShipBehaviour>().GetShip().SetAffectedByStorm(false);
             }
         }
+        #endregion
     }
 }
