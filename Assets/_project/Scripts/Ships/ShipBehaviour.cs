d6c1601017b68310776c678954fe648304e8b48a
Corentinrmt <corentin.remot@gmail.com>
2025-10-14 16:02:02 +0200

    [FIX] fix wind indicator

--- Assets/_project/Scripts/Ships/ShipBehaviour.cs
+++ Assets/_project/Scripts/Ships/ShipBehaviour.cs
@@ -348,6 +348,7 @@ namespace AltCtrl.Charybdis
             if (_affectedByWind)
             {
                 targetVelocity += new Vector2(_windDirModifier.x, _windDirModifier.y) * _data.WindForceMultiplier;
+                Debug.DrawRay(transform.position, _windDirModifier * _data.WindForceMultiplier, Color.red, 1f);
             }
 
             _rb.linearVelocity = Vector2.Lerp(_rb.linearVelocity, targetVelocity, Time.fixedDeltaTime * _data.Acceleration);
@@ -392,6 +393,7 @@ namespace AltCtrl.Charybdis
             if (_affectedByWind)
             {
                 targetVelocity += new Vector2(_windDirModifier.x, _windDirModifier.y) * _data.WindForceMultiplier;
+                Debug.DrawRay(transform.position, _windDirModifier * _data.WindForceMultiplier, Color.red, 1f);
             }
 
             _rb.linearVelocity = Vector2.Lerp(_rb.linearVelocity, targetVelocity, Time.fixedDeltaTime * _data.Acceleration);
