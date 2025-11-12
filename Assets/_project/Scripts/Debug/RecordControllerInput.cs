a16d2f22c4230eda5bef623056dd332aee2972dd
Corentinrmt <corentin.remot@gmail.com>
2025-11-12 17:33:27 +0100

    [UPDATE] fix issues with inputs joysticks

---
+++ Assets/_project/Scripts/Debug/RecordControllerInput.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class RecordControllerInput : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI _uiText;
+    private string[] joystickNames;
+    private float xAxis, yAxis;
+    private List<string> lastPressedKeys = new List<string>();
+
+    void Update()
+    {
+        joystickNames = Input.GetJoystickNames();
+        xAxis = Input.GetAxis("Horizontal");
+        yAxis = Input.GetAxis("Vertical");
+
+        foreach (KeyCode curKey in Enum.GetValues(typeof(KeyCode)))
+        {
+            if (Input.GetKeyDown(curKey))
+            {
+                lastPressedKeys.Add(curKey.ToString());
+                if (lastPressedKeys.Count > 10)
+                    lastPressedKeys.RemoveAt(0);
+            }
+        }
+
+        _uiText.text = $"Axis x = {xAxis} , y = {yAxis}";
+    }
+
+    private void OnGUI()
+    {
+        GUILayout.Label("Joysticks:");
+        foreach (var curName in joystickNames)
+            GUILayout.Label(string.Format("   {0}", curName));
+        GUILayout.Label(string.Format("Axes: ({0}, {1})", xAxis, yAxis));
+
+        GUILayout.Label("Last pressed keys:");
+        foreach (var curKeyName in lastPressedKeys)
+            GUILayout.Label(string.Format("   {0}", curKeyName));
+    }
+}
