7156cd408efe89f5be0d64bebff1e96a03108385
CorentinRmt <corentin.remot@gmail.com>
2026-02-22 19:16:36 +0100

    [FIX] fix issue wrong values used for god/human points multiplier

---
+++ Assets/_project/Scripts/Tuto/TutoController_Human.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+namespace AltCtrl.Charybdis
+{
+    public class TutoController_Human : MonoBehaviour
+    {
+        #region Fields
+
+
+        #endregion
+
+        #region Properties
+
+
+        #endregion
+
+    }
+}
